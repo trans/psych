c3b8cfb93fce97f20682b3cbda6c9c00f86a0240
7rans <transfire@gmail.com>
2013-02-01 01:21:47 -0500

    * lib/psych/schema/: use tag instead of add.

--- lib/psych/schema/failsafe_schema.rb
+++ lib/psych/schema/failsafe_schema.rb
@@ -1,15 +1,18 @@
 module Psych
 
   # The set of core YAML tag types.
-  FAILSAFE_SCHEMA = Schema.new do |ts|
+  #
+  # TODO: This should be in *all* Schema.
+  #
+  FAILSAFE_SCHEMA = Schema.new do |s|
     # tag:yaml.org,2002:str
-    ts.add '!!str', String
+    s.tag '!!str', String
 
     # tag:yaml.org,2002:map
-    ts.add '!!map', Hash
+    s.tag '!!map', Hash
 
     # tag:yaml.org,2002:seq
-    ts.add '!!seq', Array
+    s.tag '!!seq', Array
   end
 
 end
--- lib/psych/schema/json_schema.rb
+++ lib/psych/schema/json_schema.rb
@@ -1,40 +1,40 @@
 module Psych
 
   # The set of extra YAML tags.
-  JSON_SCHEMA = Schema.new do |ts|
+  JSON_SCHEMA = Schema.new do |s|
 
     # tag:yaml.org,2002:float
-    ts.add '!!float' do |tag, value|
+    s.tag '!!float' do |tag, value|
       value  # TODO: string scanner takes care of it?
     end
 
     # tag:yaml.org,2002:int
-    ts.add '!!int' do |tag, value|
+    s.tag '!!int' do |tag, value|
       value  # TODO: string scanner takes care of it?
     end
 
     # tag:yaml.org,2002:null
-    ts.add '!!null' do |tag, value|
+    s.tag '!!null' do |tag, value|
       nil
     end
 
     # tag:yaml.org,2002:bool
-    ts.add '!!bool' do |tag, value|
+    s.tag '!!bool' do |tag, value|
       !!value
     end
 
     # tag:yaml.org,2002:binary
-    ts.add '!!binary' do |tag, value|
+    s.tag '!!binary' do |tag, value|
       value.unpack('m').first
     end
 
     # tag:yaml.org,2002:omap
-    ts.add '!!omap', Psych::Omap
+    s.tag '!!omap', Psych::Omap
 
     # TODO: why not Ruby's set class?
 
     # tag:yaml.org,2002:set  
-    ts.add '!!set', Psych::Set
+    s.tag '!!set', Psych::Set
   end
 
 end
