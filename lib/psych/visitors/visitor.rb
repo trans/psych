f61de876fb8e8b48d1094c57b3103f6fe6649492
7rans <transfire@gmail.com>
2013-02-02 17:01:25 -0500

    lib/psych/visitors/visitor.rb: Remove stumpy comment.

--- lib/psych/visitors/visitor.rb
+++ lib/psych/visitors/visitor.rb
@@ -14,9 +14,6 @@ module Psych
       def visit target
         msg = DISPATCH[target.class]
 
-        # FIXME: okay I am stumped here. Where are these method supposed to be getting defined?
-        return target unless respond_to? msg
-
         send msg, target
       end
     end
