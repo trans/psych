8ee457a54e0144dedea781df724ba703cdcba115
7rans <transfire@gmail.com>
2013-02-04 14:26:17 -0500

    * lib/psych/core_ext/exception.rb: Handle instance variables for deserialized Exception.

--- lib/psych/core_ext/exception.rb
+++ lib/psych/core_ext/exception.rb
@@ -2,7 +2,13 @@ class Exception
   def self.new_with(coder)
     case coder.type
     when :map
-      exception(coder.map['message'])
+      err = exception(coder.map['message'])
+      coder.map.each do |k,v|
+        next if k == 'message'
+        iv = k.to_s.start_with?('@') ? k : "@#{k}"
+        err.instance_variable_set(iv, v)
+      end
+      err
     else
       exception(coder.scalar)
     end
