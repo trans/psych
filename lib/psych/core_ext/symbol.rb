25f452187610fc986566904e9613870458a623aa
7rans <transfire@gmail.com>
2013-02-04 19:40:23 -0500

    Fix Symbol.new_with to actually produce symbols.

--- lib/psych/core_ext/symbol.rb
+++ lib/psych/core_ext/symbol.rb
@@ -2,9 +2,9 @@ class Symbol
   def self.new_with(coder)
     str = coder.scalar.to_s
     if str.start_with?(':')
-      str[1..-1].to_str
+      str[1..-1].to_sym
     else
-      str.to_str
+      str.to_sym
     end
   end
 end
