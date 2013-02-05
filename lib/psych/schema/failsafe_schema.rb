3c7765ca3629331c1aa094f008d4418b4d0c4724
7rans <transfire@gmail.com>
2013-02-05 11:52:11 -0500

    FAILSAFE_SCHEMA does not need to redefine core tags.

--- lib/psych/schema/failsafe_schema.rb
+++ lib/psych/schema/failsafe_schema.rb
@@ -1,18 +1,12 @@
 module Psych
 
-  # The set of core YAML tag types.
-  #
-  # TODO: This should be in *all* Schema.
+  # The set of core YAML tag types *only*.
   #
   FAILSAFE_SCHEMA = Schema.new do |s|
-    # tag:yaml.org,2002:str
-    s.tag '!!str', String
-
-    # tag:yaml.org,2002:map
-    s.tag '!!map', Hash
-
-    # tag:yaml.org,2002:seq
-    s.tag '!!seq', Array
+    # These are the default tags of all Schema.
+    # s.tag '!!str', String  # tag:yaml.org,2002:str
+    # s.tag '!!map', Hash    # tag:yaml.org,2002:map
+    # s.tag '!!seq', Array   # tag:yaml.org,2002:seq
   end
 
 end
