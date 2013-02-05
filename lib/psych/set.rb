f4de309fab1336299082060c5989a0a6d9b81b74
7rans <transfire@gmail.com>
2013-02-05 11:55:13 -0500

    Document Psych::Set a little better.

--- lib/psych/set.rb
+++ lib/psych/set.rb
@@ -1,8 +1,17 @@
 module Psych
+  ###
+  # Psych's Set class is little more than a Hash.
+  # 
+  # TODO: Why not use Ruby's own Set class?
  class Set < ::Hash
+    ###
+    # Initialize Set instance using a Coder.
+    #
+    # coder - Coder instance. [Coder]
+    #
+    # Returns nothing.
     def init_with(coder)
       replace coder.map
     end
   end
 end
-
